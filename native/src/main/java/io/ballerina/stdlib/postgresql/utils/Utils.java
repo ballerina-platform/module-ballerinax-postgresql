951b2a18469b6212e0c12112154d72dcf52ab05f
Kalaiyarasi <ashakalai31@gmail.com>
2022-05-20 15:49:21 +0530

    Upgrade jdbc driver version to 42.3.3

--- native/src/main/java/io/ballerina/stdlib/postgresql/utils/Utils.java
+++ native/src/main/java/io/ballerina/stdlib/postgresql/utils/Utils.java
@@ -18,6 +18,7 @@
 package io.ballerina.stdlib.postgresql.utils;
 
 import io.ballerina.runtime.api.creators.ValueCreator;
+import io.ballerina.runtime.api.utils.StringUtils;
 import io.ballerina.runtime.api.values.BDecimal;
 import io.ballerina.runtime.api.values.BMap;
 import io.ballerina.runtime.api.values.BString;
@@ -28,8 +29,9 @@ import io.ballerina.stdlib.postgresql.Constants;
  */
 public class Utils {
     public static BMap generateOptionsMap(BMap postgresqlOptions) {
+        BMap<BString, Object> options = ValueCreator.createMapValue();
+        options.put(StringUtils.fromString("quoteReturningIdentifiers"), false);
         if (postgresqlOptions != null) {
-            BMap<BString, Object> options = ValueCreator.createMapValue();    
             addSSLOptions(postgresqlOptions.getMapValue(Constants.Options.SSL), options);
             long connectTimeout = getTimeout(postgresqlOptions.get(Constants.Options.CONNECT_TIMEOUT_SECONDS));
             if (connectTimeout >= 0) {
