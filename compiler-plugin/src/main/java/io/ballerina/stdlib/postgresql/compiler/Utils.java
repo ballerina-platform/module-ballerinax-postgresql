dedf474413c650324eaba3c3235e4d4eec22a234
Kalaiyarasiganeshalingam <ashakalai31@gmail.com>
2023-06-16 16:59:48 +0530

    Fix spread-field error in connection pool config

--- compiler-plugin/src/main/java/io/ballerina/stdlib/postgresql/compiler/Utils.java
+++ compiler-plugin/src/main/java/io/ballerina/stdlib/postgresql/compiler/Utils.java
@@ -18,24 +18,38 @@
 package io.ballerina.stdlib.postgresql.compiler;
 
 import io.ballerina.compiler.api.symbols.ModuleSymbol;
+import io.ballerina.compiler.api.symbols.Symbol;
+import io.ballerina.compiler.api.symbols.SymbolKind;
 import io.ballerina.compiler.api.symbols.TypeDescKind;
 import io.ballerina.compiler.api.symbols.TypeReferenceTypeSymbol;
 import io.ballerina.compiler.api.symbols.TypeSymbol;
 import io.ballerina.compiler.api.symbols.UnionTypeSymbol;
 import io.ballerina.compiler.syntax.tree.BasicLiteralNode;
+import io.ballerina.compiler.syntax.tree.ChildNodeEntry;
 import io.ballerina.compiler.syntax.tree.ExpressionNode;
 import io.ballerina.compiler.syntax.tree.MappingConstructorExpressionNode;
 import io.ballerina.compiler.syntax.tree.MappingFieldNode;
+import io.ballerina.compiler.syntax.tree.ModulePartNode;
 import io.ballerina.compiler.syntax.tree.Node;
-import io.ballerina.compiler.syntax.tree.SeparatedNodeList;
+import io.ballerina.compiler.syntax.tree.NodeList;
+import io.ballerina.compiler.syntax.tree.NonTerminalNode;
+import io.ballerina.compiler.syntax.tree.RecordFieldNode;
+import io.ballerina.compiler.syntax.tree.RecordFieldWithDefaultValueNode;
+import io.ballerina.compiler.syntax.tree.RecordTypeDescriptorNode;
+import io.ballerina.compiler.syntax.tree.SimpleNameReferenceNode;
 import io.ballerina.compiler.syntax.tree.SpecificFieldNode;
+import io.ballerina.compiler.syntax.tree.SpreadFieldNode;
+import io.ballerina.compiler.syntax.tree.TypeDefinitionNode;
+import io.ballerina.compiler.syntax.tree.TypedBindingPatternNode;
 import io.ballerina.compiler.syntax.tree.UnaryExpressionNode;
 import io.ballerina.projects.plugins.SyntaxNodeAnalysisContext;
 import io.ballerina.tools.diagnostics.Diagnostic;
 import io.ballerina.tools.diagnostics.DiagnosticFactory;
 import io.ballerina.tools.diagnostics.DiagnosticInfo;
 import io.ballerina.tools.diagnostics.DiagnosticSeverity;
+import io.ballerina.tools.diagnostics.Location;
 
+import java.util.List;
 import java.util.Optional;
 
 import static io.ballerina.stdlib.postgresql.compiler.Constants.UNNECESSARY_CHARS_REGEX;
@@ -101,45 +115,171 @@ public class Utils {
                 return false;
         }
     }
-
-    public static void validateOptions(SyntaxNodeAnalysisContext ctx, MappingConstructorExpressionNode options) {
-        SeparatedNodeList<MappingFieldNode> fields = options.fields();
-        for (MappingFieldNode field : fields) {
-            String name = ((SpecificFieldNode) field).fieldName().toString()
-                    .trim().replaceAll(UNNECESSARY_CHARS_REGEX, "");
-            ExpressionNode valueNode = ((SpecificFieldNode) field).valueExpr().get();
-            switch (name) {
-                case Constants.Options.CONNECT_TIMEOUT:
-                case Constants.Options.LOGIN_TIMEOUT:
-                case Constants.Options.SOCKET_TIMEOUT:
-                case Constants.Options.CANCEL_SIGNAL_TIMEOUT:
-                    float timeoutVal = Float.parseFloat(getTerminalNodeValue(valueNode, "0"));
-                    if (timeoutVal < 0) {
-                        DiagnosticInfo diagnosticInfo = new DiagnosticInfo(POSTGRESQL_101.getCode(),
-                                POSTGRESQL_101.getMessage(), POSTGRESQL_101.getSeverity());
-                        ctx.reportDiagnostic(
-                                DiagnosticFactory.createDiagnostic(diagnosticInfo, valueNode.location()));
+    public static void  validateOptionConfig(SyntaxNodeAnalysisContext ctx, MappingConstructorExpressionNode options) {
+        for (MappingFieldNode field: options.fields()) {
+            if (field instanceof SpecificFieldNode) {
+                SpecificFieldNode specificFieldNode = ((SpecificFieldNode) field);
+                validateOptions(ctx, specificFieldNode.fieldName().toString().trim().
+                        replaceAll(UNNECESSARY_CHARS_REGEX, ""), specificFieldNode.valueExpr().get());
+            } else if (field instanceof SpreadFieldNode) {
+                NodeList<Node> recordFields = Utils.getSpreadFieldType(ctx, ((SpreadFieldNode) field));
+                for (Node recordField : recordFields) {
+                    if (recordField instanceof RecordFieldWithDefaultValueNode) {
+                        RecordFieldWithDefaultValueNode fieldWithDefaultValueNode =
+                                (RecordFieldWithDefaultValueNode) recordField;
+                        validateOptions(ctx, fieldWithDefaultValueNode.fieldName().toString().
+                                        trim().replaceAll(UNNECESSARY_CHARS_REGEX, ""),
+                                fieldWithDefaultValueNode.expression());
                     }
-                    break;
-                case Constants.Options.ROW_FETCH_SIZE:
-                case Constants.Options.CACHED_METADATA_FIELD_COUNT:
-                case Constants.Options.CACHED_METADATA_FIELD_SIZE:
-                case Constants.Options.PREPARED_STATEMENT_THRESHOLD:
-                case Constants.Options.PREPARED_STATEMENT_CACHE_QUERIES:
-                case Constants.Options.PREPARED_STATEMENT_CACHE_SIZE_MIB:
-                    int sizeVal = Integer.parseInt(getTerminalNodeValue(valueNode, "1"));
-                    if (sizeVal <= 0) {
-                        DiagnosticInfo diagnosticInfo = new DiagnosticInfo(POSTGRESQL_102.getCode(),
-                                POSTGRESQL_102.getMessage(), POSTGRESQL_102.getSeverity());
-                        ctx.reportDiagnostic(
-                                DiagnosticFactory.createDiagnostic(diagnosticInfo, valueNode.location()));
-                    }
-                    break;
-                default:
-                    // Can ignore all the other fields
-                    continue;
+                }
             }
         }
+    }
+
+    public static void validateOptions(SyntaxNodeAnalysisContext ctx, String name, ExpressionNode valueNode) {
+        switch (name) {
+            case Constants.Options.CONNECT_TIMEOUT:
+            case Constants.Options.LOGIN_TIMEOUT:
+            case Constants.Options.SOCKET_TIMEOUT:
+            case Constants.Options.CANCEL_SIGNAL_TIMEOUT:
+                float timeoutVal = Float.parseFloat(getTerminalNodeValue(valueNode, "0"));
+                if (timeoutVal < 0) {
+                    DiagnosticInfo diagnosticInfo = new DiagnosticInfo(POSTGRESQL_101.getCode(),
+                            POSTGRESQL_101.getMessage(), POSTGRESQL_101.getSeverity());
+                    ctx.reportDiagnostic(
+                            DiagnosticFactory.createDiagnostic(diagnosticInfo, valueNode.location()));
+                }
+                break;
+            case Constants.Options.ROW_FETCH_SIZE:
+            case Constants.Options.CACHED_METADATA_FIELD_COUNT:
+            case Constants.Options.CACHED_METADATA_FIELD_SIZE:
+            case Constants.Options.PREPARED_STATEMENT_THRESHOLD:
+            case Constants.Options.PREPARED_STATEMENT_CACHE_QUERIES:
+            case Constants.Options.PREPARED_STATEMENT_CACHE_SIZE_MIB:
+                int sizeVal = Integer.parseInt(getTerminalNodeValue(valueNode, "1"));
+                if (sizeVal <= 0) {
+                    DiagnosticInfo diagnosticInfo = new DiagnosticInfo(POSTGRESQL_102.getCode(),
+                            POSTGRESQL_102.getMessage(), POSTGRESQL_102.getSeverity());
+                    ctx.reportDiagnostic(
+                            DiagnosticFactory.createDiagnostic(diagnosticInfo, valueNode.location()));
+                }
+                break;
+            default:
+                // Can ignore all the other fields
+        }
+    }
+
+    public static NodeList<Node> getSpreadFieldType(SyntaxNodeAnalysisContext ctx, SpreadFieldNode spreadFieldNode) {
+        List<Symbol> symbols = ctx.semanticModel().moduleSymbols();
+        Object[] entries = spreadFieldNode.valueExpr().childEntries().toArray();
+        ModulePartNode modulePartNode = ctx.syntaxTree().rootNode();
+        ChildNodeEntry type = Utils.getVariableType(symbols, entries, modulePartNode);
+        RecordTypeDescriptorNode typeDescriptor = Utils.getFirstSpreadFieldRecordTypeDescriptorNode(symbols,
+                type, modulePartNode);
+        typeDescriptor = Utils.getEndSpreadFieldRecordType(symbols, entries, modulePartNode,
+                typeDescriptor);
+        return typeDescriptor.fields();
+    }
+
+    public static ChildNodeEntry getVariableType(List<Symbol> symbols, Object[] entries,
+                                                 ModulePartNode modulePartNode) {
+        for (Symbol symbol : symbols) {
+            if (!symbol.kind().equals(SymbolKind.VARIABLE)) {
+                continue;
+            }
+            Optional<String> symbolName = symbol.getName();
+            Optional<Node> childNodeEntry = ((ChildNodeEntry) entries[0]).node();
+            if (symbolName.isPresent() && childNodeEntry.isPresent() &&
+                    symbolName.get().equals(childNodeEntry.get().toString())) {
+                Optional<Location> location = symbol.getLocation();
+                if (location.isPresent()) {
+                    Location loc = location.get();
+                    NonTerminalNode node = modulePartNode.findNode(loc.textRange());
+                    if (node instanceof TypedBindingPatternNode) {
+                        TypedBindingPatternNode typedBindingPatternNode = (TypedBindingPatternNode) node;
+                        return  (ChildNodeEntry) typedBindingPatternNode.childEntries().toArray()[0];
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public static RecordTypeDescriptorNode getFirstSpreadFieldRecordTypeDescriptorNode(List<Symbol> symbols,
+                                                                                       ChildNodeEntry type,
+                                                                                       ModulePartNode modulePartNode) {
+        if (type != null && type.node().isPresent()) {
+            for (Symbol symbol : symbols) {
+                if (!symbol.kind().equals(SymbolKind.TYPE_DEFINITION)) {
+                    continue;
+                }
+                if (symbol.getName().isPresent() &&
+                        symbol.getName().get().equals(type.node().get().toString().trim())) {
+                    Optional<Location> loc = symbol.getLocation();
+                    if (loc.isPresent()) {
+                        Location location = loc.get();
+                        Node node = modulePartNode.findNode(location.textRange());
+                        if (node instanceof TypeDefinitionNode) {
+                            TypeDefinitionNode typeDefinitionNode = (TypeDefinitionNode) node;
+                            return  (RecordTypeDescriptorNode) typeDefinitionNode.typeDescriptor();
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public static RecordTypeDescriptorNode getEndSpreadFieldRecordType(List<Symbol> symbols, Object[] entries,
+                                                                       ModulePartNode modulePartNode,
+                                                                       RecordTypeDescriptorNode typeDescriptor) {
+        if (typeDescriptor != null) {
+            for (int i = 1; i < entries.length; i++) {
+                String childNodeEntry = ((ChildNodeEntry) entries[i]).node().get().toString();
+                NodeList<Node> recordFields = typeDescriptor.fields();
+                if (childNodeEntry.equals(".")) {
+                    continue;
+                }
+                for (Node recordField : recordFields) {
+                    String fieldName;
+                    Node fieldType;
+                    if (recordField instanceof RecordFieldWithDefaultValueNode) {
+                        RecordFieldWithDefaultValueNode fieldWithDefaultValueNode =
+                                (RecordFieldWithDefaultValueNode) recordField;
+                        fieldName = fieldWithDefaultValueNode.fieldName().text().trim();
+                        fieldType = fieldWithDefaultValueNode.typeName();
+                    } else {
+                        RecordFieldNode fieldNode = (RecordFieldNode) recordField;
+                        fieldName = fieldNode.fieldName().text().trim();
+                        fieldType = fieldNode.typeName();
+                    }
+                    if (fieldName.equals(childNodeEntry.trim())) {
+                        if (fieldType instanceof SimpleNameReferenceNode) {
+                            SimpleNameReferenceNode nameReferenceNode = (SimpleNameReferenceNode) fieldType;
+                            for (Symbol symbol : symbols) {
+                                if (!symbol.kind().equals(SymbolKind.TYPE_DEFINITION)) {
+                                    continue;
+                                }
+                                if (symbol.getName().isPresent() &&
+                                        symbol.getName().get().equals(nameReferenceNode.name().text().trim())) {
+                                    Optional<Location> loc = symbol.getLocation();
+                                    if (loc.isPresent()) {
+                                        Location location = loc.get();
+                                        Node node = modulePartNode.findNode(location.textRange());
+                                        if (node instanceof TypeDefinitionNode) {
+                                            TypeDefinitionNode typeDefinitionNode = (TypeDefinitionNode) node;
+                                            typeDescriptor = (RecordTypeDescriptorNode) typeDefinitionNode.
+                                                    typeDescriptor();
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return typeDescriptor;
     }
 
     public static String getTerminalNodeValue(Node valueNode, String defaultValue) {
